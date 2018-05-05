74facd95305fe3b765e25a1dd5e82283acb43dfc
Aniqa Rehman <raniqa57@gmail.com>
2018-05-05 13:23:20 +0200

    Practical HTML personalization

---
+++ Lab 1 - HTML Personalization/widget.js
@@ -0,0 +1,26 @@
+$(document).on('click', '.panel-heading span.icon_minim', function (e) {
+    var $this = $(this);
+    if (!$this.hasClass('panel-collapsed')) {
+        $this.parents('.panel').find('.panel-body').slideUp();
+        $this.addClass('panel-collapsed');
+        $this.removeClass('glyphicon-minus').addClass('glyphicon-plus');
+    } else {
+        $this.parents('.panel').find('.panel-body').slideDown();
+        $this.removeClass('panel-collapsed');
+        $this.removeClass('glyphicon-plus').addClass('glyphicon-minus');
+    }
+});
+$(document).on('focus', '.panel-footer input.chat_input', function (e) {
+    var $this = $(this);
+    if ($('#minim_chat_window').hasClass('panel-collapsed')) {
+        $this.parents('.panel').find('.panel-body').slideDown();
+        $('#minim_chat_window').removeClass('panel-collapsed');
+        $('#minim_chat_window').removeClass('glyphicon-plus').addClass('glyphicon-minus');
+    }
+});
+
+$(document).on('click', '#login', function (e) {
+    var loader = "<div id='loader' class='loader' style=' margin: 0 auto;'></div>"
+    $("#widget-container").replaceWith(loader);
+});
+
